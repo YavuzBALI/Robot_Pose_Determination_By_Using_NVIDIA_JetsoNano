a39280d9eb3a9feba39a46ffb794dd17b282640b
Yavuz BALI <84620286+YavuzBALI@users.noreply.github.com>
2021-08-03 14:40:08 +0300

    Create Covert_TF-TRT.py

---
+++ Covert_TF-TRT.py
@@ -0,0 +1,33 @@
+import tensorflow as tf 
+from tensorflow.python.compiler.tensorrt import trt_convert as trt 
+import numpy as np
+
+#Saved model loaded to convert
+saved_model_dir='101x101_image_size/Robot_Angle_pre_SavedModel_101_colab'
+
+# Setting convert paramater
+conversion_param = trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(
+    precision_mode=trt.TrtPrecisionMode.FP16,
+    max_workspace_size_bytes=80000000
+)
+
+#An offline converter for TF-TRT transformation for TF 2.0 SavedModels.
+#Currently this is not available on Windows platform.
+converter=trt.TrtGraphConverterV2(
+    input_saved_model_dir=saved_model_dir,
+    conversion_params=conversion_param
+)
+#Convert model TensorRT optimizer
+converter.convert()
+
+#Run inference with converted graph in order to build TensorRT engines.
+def my_input_fn():
+  inp1 = np.random.normal(size=(1, 101, 101, 3)).astype(np.float32)
+  yield [inp1]
+converter.build(input_fn=my_input_fn())
+
+#The converted model is saved
+saved_model_dir_trt='Robot_Angle_pre_SavedModel_101x101_FP16_build_3'
+converter.save(saved_model_dir_trt)
+
+print('***********************Conversion is Done********************')
